c740c0f6e2b2cca44f4bb919e5f20297f774fb83
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2016-04-07 23:40:03 +0900

    Repository of Webupd8 added
    Oracle Java Development Kit and Sublime Text 3's repository was added.
    JDK8 and Sublime Text 3 will going to be installed automatically.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 #   Run this script with "sudo"
 ###
 
+## Add apt-repository from Webupd8(www.webupd8.org)
+# Oracle Java Development Kit Repos
+sudo add-apt-repository ppa:webupd8team/java
+# Sublime Text 3 Repos
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
 ## Update your repository database
 sudo apt-get update
 
@@ -12,3 +18,10 @@ sudo apt-get install nodejs npm
 ## update Node.js command from nodejs to node
 # Source from StackOverflow: http://stackoverflow.com/questions/18130164/nodejs-vs-node-on-ubuntu-12-04
 sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
+
+## Install Repos added in the beginning
+# Install JDK 8
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
+# Install Sublime Text 3
+sudo apt-get install sublime-text-installer
