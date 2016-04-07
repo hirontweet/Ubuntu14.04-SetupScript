598a20637be772d6800db264bc1e659dd1752591
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2016-04-07 23:32:43 +0900

    Added updating repository database and installing node related stuffs.
    nodejs and npm are going to be installed.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 #   Run this script with "sudo"
 ###
 
+## Update your repository database
+sudo apt-get update
+
+## Install nodejs
+sudo apt-get install nodejs npm
+
 ## update Node.js command from nodejs to node
 # Source from StackOverflow: http://stackoverflow.com/questions/18130164/nodejs-vs-node-on-ubuntu-12-04
 sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
